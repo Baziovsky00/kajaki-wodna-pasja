08092840c47e9fd06b6df73dec8e507ca3c9aca4
player0307@wp.pl <player0307@wp.pl>
2025-06-18 13:31:45 +0200

    push changes

--- src/app/components/TrasyClient.tsx
+++ src/app/components/TrasyClient.tsx
@@ -1,7 +1,7 @@
 "use client"
 
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card"
-import { ArrowLeft, Clock, MapPin, Star, Trees, Castle } from "lucide-react"
+import { ArrowLeft, Clock, MapPin, Star, Trees, Castle } from "lucide-react";
 import Link from "next/link"
 import { motion } from "framer-motion"
 import Navigation from "./Navigation"
